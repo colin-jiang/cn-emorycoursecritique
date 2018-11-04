cdb4579dd35bf4d228f7b9bec7e64b8a1e7abd7a
lexa98 <axel.chauvin@emory.edu>
2018-11-04 01:32:43 -0500

    change autofill json

--- backend/service/update-keywords.js
+++ backend/service/update-keywords.js
@@ -4,26 +4,26 @@ var Rating = require(path.join(__dirname,'..','/models/ratings.js'));
 var Professor = require(path.join(__dirname,'..','/models/professor.js'));
 
 module.exports=function(req,res,next){
-    Course.find().then(function(courses) {
-        courses.forEach(function(course) {
-            var new_course = new Course({
-                course_num: course.course_num,
-                course_name: course.course_name,
-                dept: course.dept,
-                credits: course.credits,
-                ger: course.ger,
-                opus_id: course.opus_id,
-                professors: course.professors,
-                description: course.description,
-                ratings:course.ratings
-            });
-            Course.remove({_id: course.id}, function(err) {
+    // Course.find().then(function(courses) {
+    //     courses.forEach(function(course) {
+    //         var new_course = new Course({
+    //             course_num: course.course_num,
+    //             course_name: course.course_name,
+    //             dept: course.dept,
+    //             credits: course.credits,
+    //             ger: course.ger,
+    //             opus_id: course.opus_id,
+    //             professors: course.professors,
+    //             description: course.description,
+    //             ratings:course.ratings
+    //         });
+    //         Course.remove({_id: course.id}, function(err) {
 
-            });
-            new_course.save();
-            log(new_course.name);   
-        });
-    });
+    //         });
+    //         new_course.save();
+    //         console.log(new_course.name);   
+    //     });
+    // });
 
     //WARNING: MESSING WITH DB iterate through all courses and add course_num+course_name keyword -- only run once
     
